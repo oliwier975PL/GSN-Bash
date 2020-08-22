aea702cc4d075292c93833638a87491d3fffefbf
oliwier975PL <oliwier975PL@live.com>
2020-08-22 19:22:07 +0200

    Code optimization

--- GSN.sh
+++ GSN.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION='1.0.0-Bash'
+VERSION='1.0.1-Bash'
 AUTHOR='oliwier975PL & workonfire'
 
 echo -ne "\033]0;GSN\007"
@@ -9,9 +9,10 @@ echo -e "\e[93m (       )\\ )  ( /(  "
 echo -e "\e[33m )\\ )   (()/(  )\\()) "
 echo -e "\e[91m(()/(    /(_))((_)\\  "
 echo -e "\e[31m /(_))_ (_))   _((_) "
-echo -e "\e[31m(_)) __|/ __| | \\| | "
-echo -e "\e[31m  | (_ |\\__ \\ | .\` | "
-echo -e "\e[31m   \\___||___/ |_|\\_| "
+echo -e "(_)) __|/ __| | \\| | "
+echo -e "  | (_ |\\__ \\ | .\` | "
+echo -e "   \\___||___/ |_|\\_| \e[39m\n"
+
 echo "Generator Spierdolonych Nicków v${VERSION}"
 echo -e "\e[93mby ${AUTHOR}\e[39m\n"
 
@@ -56,8 +57,13 @@ read generate_number
 echo -n "Usunąć polskie znaki? (y/N): "
 read strip_polish_chars
 
-readarray -t adjectives < dictionaries/adjectives.txt
-readarray -t nouns < dictionaries/nouns.txt
+if [[ ${strip_polish_chars,} == 'y' ]]; then
+ adjectives=(`iconv -f utf-8 -t ascii//translit dictionaries/adjectives.txt`)
+ nouns=(`iconv -f utf-8 -t ascii//translit dictionaries/nouns.txt`)
+else
+ readarray -t adjectives < dictionaries/adjectives.txt
+ readarray -t nouns < dictionaries/nouns.txt
+fi
 
 generated_nicknames=()
 echo -e "Generowanie nicków...\n"
@@ -77,16 +83,13 @@ for ((i=1; i<=how_many_times; i++)); do
  if [[ ${#generated_nick} -gt $max_length ]]; then
   generated_nick="${generated_nick:0:$max_length}"
  fi
- if [[ ${strip_polish_chars,} == 'y' ]]; then
-  generated_nick=$(echo "$generated_nick" | iconv -f utf-8 -t ascii//translit)
- fi
  generated_nicknames+=( $generated_nick )
- echo -e "\e[96m> $generated_nick"
+ echo -e "\e[96m>> $generated_nick"
 done
 
 echo -e "\n\e[92mGotowe."
 
 if [[ ${save_to_file,} == 'y' ]]; then
  printf '%s\n' "${generated_nicknames[@]}" > output.txt
- echo -e "\e[92mZapisano nicki do pliku."
+ echo -e "Zapisano nicki do pliku.\e[39m"
 fi
